3065d03d86304fcd7dd4eafa1c59f72237a3c31d
Andy <andy.ahmedov2@gmail.com>
2024-01-27 15:05:58 +0300

    Fixed some errors when routing requests.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
 
-	"github.com/andy-ahmedov/crud_service/internal/domain"
 	"github.com/andy-ahmedov/crud_service/internal/repository/psql"
 	"github.com/andy-ahmedov/crud_service/internal/service"
 	"github.com/andy-ahmedov/crud_service/internal/transport/rest"
@@ -35,54 +33,4 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
-
-	tiime := time.Now()
-	book := domain.Book{
-		Title:       "Something",
-		Author:      "Dan Brown",
-		PublishDate: tiime,
-		Rating:      5,
-	}
-
-	err = booksService.Create(context.Background(), &book)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(book.ID)
-
-	bookOne, err := booksService.GetByID(context.TODO(), 1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(bookOne)
-
-	// err = db.Delete(context.TODO(), 5)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	title := "Tommy"
-	author := "JimBim"
-	rating := 3
-	tiime = time.Now()
-
-	updBook := domain.UpdateBookInput{
-		Title:       &title,
-		Author:      &author,
-		PublishDate: &tiime,
-		Rating:      &rating,
-	}
-
-	err = booksService.Update(context.TODO(), 6, updBook)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	books, err := booksService.GetAll(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(books)
-
 }
--- internal/transport/rest/handler.go
+++ internal/transport/rest/handler.go
@@ -14,14 +14,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// type BooksInterface interface {
-// 	Create(ctx context.Context, book *domain.Book) error
-// 	GetByID(ctx context.Context, id int64) (domain.Book, error)
-// 	GetAll(ctx context.Context) ([]domain.Book, error)
-// 	Delete(ctx context.Context, id int64) error
-// 	Update(ctx context.Context, id int64, updBook domain.UpdateBookInput) error
-// }
-
 type Handler struct {
 	booksService service.BooksInterface
 }
@@ -40,9 +32,9 @@ func (h Handler) InitRouter() *mux.Router {
 	{
 		books.HandleFunc("", h.createBook).Methods(http.MethodPost)
 		books.HandleFunc("", h.getAllBooks).Methods(http.MethodGet)
-		books.HandleFunc("/{id: [0-9]+}", h.getBookByID).Methods(http.MethodGet)
-		books.HandleFunc("/{id: [0-9]+}", h.deleteBook).Methods(http.MethodDelete)
-		books.HandleFunc("/{id: [0-9]+}", h.updateBook).Methods(http.MethodPut)
+		books.HandleFunc("/{id:[0-9]+}", h.getBookByID).Methods(http.MethodGet)
+		books.HandleFunc("/{id:[0-9]+}", h.deleteBook).Methods(http.MethodDelete)
+		books.HandleFunc("/{id:[0-9]+}", h.updateBook).Methods(http.MethodPut)
 	}
 
 	return r
