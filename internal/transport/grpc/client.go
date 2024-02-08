005c699d69b971c49feac299e26505bf44dd54d4
Andy <andy.ahmedov2@gmail.com>
2024-02-08 18:46:12 +0300

    Add SendLogRequest method

--- internal/transport/grpc/client.go
+++ internal/transport/grpc/client.go
@@ -1,10 +1,12 @@
 package grpc
 
 import (
+	"context"
 	"fmt"
 
 	audit "github.com/andy-ahmedov/audit_log_server/pkg/domain"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type Client struct {
@@ -32,4 +34,24 @@ func NewClient(port int) (*Client, error) {
 
 func (c *Client) CloseConnection() error {
 	return c.conn.Close()
+}
+
+func (c *Client) SendLogRequest(ctx context.Context, req audit.LogItem) error {
+	action, err := audit.ToPbAction(req.Action)
+	if err != nil {
+		return err
+	}
+	entity, err := audit.ToPbEntity(req.Entity)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.auditClient.Log(ctx, &audit.LogRequest{
+		Action:    action,
+		Entity:    entity,
+		EntityId:  req.EntityID,
+		Timestamp: timestamppb.New(req.Timestamp),
+	})
+
+	return err
 }
